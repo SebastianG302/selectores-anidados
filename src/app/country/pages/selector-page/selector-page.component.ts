a55ea3aa89038a7f78e44b0a8cf9d37c7e26bc3d
SebastianG302 <johan.guerrero@pi.edu.co>
2023-06-03 11:15:49 -0500

    pais selector finished

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { Observable, pipe, switchMap } from 'rxjs';
+import { Observable, pipe, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -16,7 +16,7 @@ export class SelectorPageComponent implements OnInit{
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
-    borders: ['', Validators.required],
+    border: ['', Validators.required],
     
   })
   constructor(
@@ -35,6 +35,10 @@ export class SelectorPageComponent implements OnInit{
   onRegionChanged(): void{
     this.myForm.get('region')!.valueChanges
       .pipe(
+        tap( () => this.myForm.get('country')!.setValue('') ),//formatear el selector
+
+        //este operador espera un observable para retornar otro.
+        //Util en caso de que se hagan muchas peticiones para evitar que sobrepongan las peticiones 
         switchMap(region => this.countriesService.getCountriesByRegion(region))
       )
       .subscribe(country => {
